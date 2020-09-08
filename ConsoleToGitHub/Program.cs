3424ba6e153cc8adaac145730cdba15425a5bf4e
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-08 10:51:17 +0200

    added a massage

--- ConsoleToGitHub/Program.cs
+++ ConsoleToGitHub/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleToGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I Love to Code!");
         }
     }
 }
