fb3f44a0109d05db6e0656cc5e53c430a4969bb8
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2020-09-08 10:48:31 +0200

    Add project files.

---
+++ ConsoleToGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleToGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
